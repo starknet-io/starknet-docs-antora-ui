8f09d328fefff3d4c727a4699774765f434f194f
xgrizx <xxxgrizxxx@gmail.com>
2023-05-10 08:48:24 +0200

    starknet updates

--- src/js/03-fragment-jumper.js
+++ src/js/03-fragment-jumper.js
@@ -18,7 +18,6 @@
       window.location.hash = '#' + this.id
       e.preventDefault()
     }
-    window.scrollTo(0, computePosition(this, 0) - toolbar.getBoundingClientRect().bottom)
   }
 
   window.addEventListener('load', function jumpOnLoad (e) {
