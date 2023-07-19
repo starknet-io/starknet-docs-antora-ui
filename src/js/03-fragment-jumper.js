aff7cae71ab9ea0733e88327c3c3a60809fd2bff
Steve Goodman <steve@starkware.co>
2023-07-19 13:07:40 +0300

    Copying latest changes from `master` on the starknet-docs repo (https://github.com/starknet-io/starknet-docs/tree/dev/ui)

--- src/js/03-fragment-jumper.js
+++ src/js/03-fragment-jumper.js
@@ -18,6 +18,7 @@
       window.location.hash = '#' + this.id
       e.preventDefault()
     }
+    window.scrollTo(0, computePosition(this, 0) - toolbar.getBoundingClientRect().bottom)
   }
 
   window.addEventListener('load', function jumpOnLoad (e) {
